d23dff161dde2f11d4188ceff7e02aeebec89d75
Josue_Tejada <omar14tejada@gmail.com>
2023-03-27 17:00:24 -0400

    dependency issue, file missing.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,27 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    'plugin:import/recommended'
-  ],
-  rules: {
-    'import/no-named-as-default-member': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  settings: {
-    'import/resolver': {
-      alias: {
-        map: [
-          ['@', './src']
-        ],
-        extensions: [".vue", ".js"]
-      }
+    root: true,
+    env: {
+      node: true
     },
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)'
-      ],
-      env: {
-        mocha: true
+    'extends': [
+      'plugin:vue/essential',
+      'eslint:recommended'
+    ],
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    },
+    parserOptions: {
+      parser: '@babel/eslint-parser'
+    },
+    overrides: [
+      {
+        files: [
+          '**/__tests__/*.{j,t}s?(x)'
+        ],
+        env: {
+          mocha: true
+        }
       }
-    }
-  ]
-}
+    ]
+  }
